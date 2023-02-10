ed8ae215cdd37f8cf8202338f121035229cbe753
narpalan <thiagofumega@gmail.com>
2023-02-10 08:04:28 -0300

    FEAT - adiciona cabeçalho responsivo.

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { AppBar } from '@mui/material';
+
+const HeaderStyled = styled(AppBar)`
+  position: static;  
+  background-color: #FAFAFA !important;
+  justify-content: center !important;
+  min-height: 10vh;
+  max-height: 12vh;  
+`;
+
+export default HeaderStyled;
