fdc9f194cd40e3dff7c7303d5acd7717993b2b35
narpalan <thiagofumega@gmail.com>
2023-02-10 08:09:29 -0300

    FEAT - integra theme provider e outros.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 
+import AppProvider from './context/Provider';
+import Main from './pages/Main';
+//import Header from './components/Header/Header';
+
+
 function App() {
+  const theme = {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <ThemeProvider theme={ theme }>
+        <Routes>
+          <Route path="/" element={ <Main/> }/>
+        </Routes>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
